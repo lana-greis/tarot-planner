789cc1dd0fa15ca826da535cc34bd4dcfa8adfc6
Alik sexy <alik.want.you@gmail.com>
2025-10-22 21:57:38 +0300

    a

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
+import { firebaseConfig } from './firebase.js'
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js"
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js"
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDQsLjfpCogYejfXUCrEAO0TsJW1g8ipMw",
-  authDomain: "lana-greis.firebaseapp.com",
-  projectId: "lana-greis",
-  storageBucket: "lana-greis.firebasestorage.app",
-  messagingSenderId: "87140146821",
-  appId: "1:87140146821:web:6ef7a4d9810352d9b253ec"
-}
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
