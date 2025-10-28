a8037d95629ecf671f9af876d454958ce2ec289e
Oardieldei <Oardieldei@users.noreply.github.com>
2025-10-28 15:13:32 +0000

    deploy: 45f225cc86d854398b9f49c17c7d06e80134cd7b

---
+++ js/firebase-template.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDQsLjfpCogYejfXUCrEAO0TsJW1g8ipMw",
+  authDomain: "lana-greis.firebaseapp.com",
+  projectId: "lana-greis",
+  storageBucket: "lana-greis.firebasestorage.app",
+  messagingSenderId: "87140146821",
+  appId: "1:87140146821:web:6ef7a4d9810352d9b253ec",
+}
